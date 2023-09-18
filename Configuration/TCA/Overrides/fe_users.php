d9ecd7107b7e18f57a71d30efd1f4469e9b2670f
Tim Schreiner <tim.schreiner@km2.de>
2023-09-18 16:38:08 +0200

    [TASK] Introduce runTests.sh and Github actions

--- Configuration/TCA/Overrides/fe_users.php
+++ Configuration/TCA/Overrides/fe_users.php
@@ -3,7 +3,7 @@
 declare(strict_types=1);
 
 /*
- * This file is part of the "academic_persons" Extension for TYPO3 CMS.
+ * This file is part of the "academic_persons_sync" Extension for TYPO3 CMS.
  *
  * For the full copyright and license information, please read the
  * LICENSE file that was distributed with this source code.
