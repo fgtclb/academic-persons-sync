a774abd7a9be22b03d6c13190c59a246df161935
Stefan Bürk <stefan@buerk.tech>
2025-03-06 16:52:03 +0100

    [TASK] Maintain dependency in `composer.json` and `ext_emconf.php`
    Used command(s):
    
    ```shell
    composer remove --dev --no-update \
      'typo3/minimal' \
    && composer require --no-update \
      'php':'^8.0 || ^8.1 || ^8.2 || ^8.3' \
      'typo3/cms-core':'^11.5' \
      'fgtclb/academic-persons':'^1.0 || 1.*.*@dev' \
      'fgtclb/academic-persons-edit':'^1.0 || 1.*.*@dev' \
    && composer require --dev --no-update \
      'typo3/cms-backend':'^11.5' \
      'typo3/cms-extbase':'^11.5' \
      'typo3/cms-fluid':'^11.5' \
      'typo3/cms-frontend':'^11.5' \
      'typo3/cms-felogin':'^11.5' \
      'typo3/cms-install':'^11.5'
    ```

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,11 +1,5 @@
 <?php
 
-/*
- * This file is part of the "academic_persons_sync" Extension for TYPO3 CMS.
- *
- * For the full copyright and license information, please read the
- * LICENSE file that was distributed with this source code.
- */
 
 $EM_CONF[$_EXTKEY] = [
     'title' => 'FGTCLB: Academic Persons Sync',
@@ -20,8 +14,8 @@ $EM_CONF[$_EXTKEY] = [
     'constraints' => [
         'depends' => [
             'typo3' => '11.5.0-11.5.99',
-            'academic_persons' => '0.2.0 - 0.2.99',
-            'academic_persons_edit' => '0.2.0 - 0.2.99',
+            'academic_persons' => '1.0.0-1.99.99',
+            'academic_persons_edit' => '1.0.0-1.99.99',
         ],
         'conflicts' => [
         ],
