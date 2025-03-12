cefc56b4fc0c280c8f4309cd9692a254169a6700
Stefan Bürk <stefan@buerk.tech>
2025-03-12 20:53:30 +0100

    [TASK] Add TYPO3 v12 support
    This change streamlines the dependencies
    along with the workflow definition to
    allow the usage with TYPO3 v12.
    
    Minor cleanups on dependencies are also
    processed.
    
    Used command(s):
    
    ```shell
    composer remove --dev --no-update \
      'andreaswolf/typo3-uuid' \
      'typo3/cms-extbase' \
    && composer require --no-update \
      'php':'^8.0 || ^8.1 || ^8.2 || ^8.3 || ^8.4' \
      'typo3/cms-core':'^11.5 || ^12.4' \
      'typo3/cms-extbase':'^11.5 || ^12.4' \
    && composer require --dev --no-update \
      'typo3/cms-backend':'^11.5 || ^12.4' \
      'typo3/cms-felogin':'^11.5 || ^12.4' \
      'typo3/cms-fluid':'^11.5 || ^12.4' \
      'typo3/cms-frontend':'^11.5 || ^12.4' \
      'typo3/cms-install':'^11.5 || ^12.4' \
      'typo3/cms-composer-installers':'v4.0.0-RC2 || ^5' \
    rm -rf patches/
    ```

--- Build/Scripts/composer-for-core-version.sh
+++ Build/Scripts/composer-for-core-version.sh
@@ -20,35 +20,35 @@ composer_update() {
     git restore composer.json
 }
 
-update_v12() {
-    echo -e "💪 Enforce TYPO3 v12"
-    composer require --no-update \
-        "typo3/cms-core":"^12.4"
-
-    echo -e "💪 Enforce PHPUnit ^10.1"
-    composer req --dev --no-update \
-        "phpunit/phpunit":"^10.1"
-}
-
 update_v11() {
     echo -e "💪 Enforce TYPO3 v11"
     composer require --no-update \
-        "typo3/cms-core":"^11.5"
+        "typo3/minimal":"^11.5"
 
     echo -e "💪 Enforce PHPUnit ^9.6.8"
     composer req --dev --no-update \
         "phpunit/phpunit":"^9.6.8"
 }
 
+update_v12() {
+    echo -e "💪 Enforce TYPO3 v12"
+    composer require --no-update \
+        "typo3/minimal":"^12.4"
+
+    echo -e "💪 Enforce PHPUnit ^10.1"
+    composer req --dev --no-update \
+        "phpunit/phpunit":"^10.1"
+}
+
 case "$1" in
-12)
-    composer_cleanup
-    update_v12
-    composer_update
-    ;;
 11)
     composer_cleanup
     update_v11
+    composer_update
+    ;;
+12)
+    composer_cleanup
+    update_v12
     composer_update
     ;;
 *)
