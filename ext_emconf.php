800ec02805ac0da504bce9a2de16c2c8bf1fc52e
Stefan Bürk <stefan@buerk.tech>
2025-05-06 18:54:57 +0200

    [RELEASE] 2.0.1

--- ext_emconf.php
+++ ext_emconf.php
@@ -8,14 +8,14 @@ $EM_CONF[$_EXTKEY] = [
     'author_email' => 'tim.schreiner@km2.de',
     'author_company' => 'FGTCLB',
     'state' => 'beta',
-    'version' => '2.0.0',
+    'version' => '2.0.1',
     'clearCacheOnLoad' => true,
     'constraints' => [
         'depends' => [
             'typo3' => '12.4.0-13.4.99',
             'extbase' => '12.4.0-13.4.99',
-            'academic_persons' => '2.0.0',
-            'academic_persons_edit' => '2.0.0',
+            'academic_persons' => '2.0.1',
+            'academic_persons_edit' => '2.0.1',
         ],
         'conflicts' => [
         ],
