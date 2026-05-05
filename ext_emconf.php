5aec62c420305200dc5991477fd9ee7561305fce
Stefan Bürk <stefan@buerk.tech>
2026-05-05 16:48:41 +0200

    [TASK] ACE-228: Normalize `ext_emconf.php`
    This changes uses the `ext_emconf.php` normalization of the
    upcoming tool `packwright` to all of the split- and test
    extension files in a good shape.
    
    Used command(s):
    
    ```bash
    pkw-dev extemconf:normalize -t 12 -- packages/**/ext_emconf.php && \
    pkw-dev extemconf:set \
      --ext-version=2.3.2 \
      --author-company='FGTCLB GmbH' \
      --author-email='hello@fgtclb.com' \
      -- packages/**/ext_emconf.php
    ```
    
    > [!NOTE]
    > `pkw (packwraight)` not realease in public yet. It's the upcoming
    > TYPO3 extension release took with a couple of helpers. Currently
    > only `ext_emconf.php` validation, normalization, value setting and
    > constraints handling is availalbe and the reason that the development
    > version has been used.
    
    This is done as preparation for the next release.

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,10 +1,14 @@
 <?php
 
 $EM_CONF[$_EXTKEY] = [
-    'author' => 'FGTCLB',
-    'author_company' => 'FGTCLB GmbH',
-    'author_email' => 'hello@fgtclb.com',
+    'title' => 'FGTCLB: Academic Persons Sync',
+    'description' => 'Adds some configuration for external users providers like Active Directory.',
+    'version' => '2.3.2',
     'category' => 'plugin',
+    'state' => 'beta',
+    'author' => 'FGTCLB',
+    'author_email' => 'hello@fgtclb.com',
+    'author_company' => 'FGTCLB GmbH',
     'constraints' => [
         'depends' => [
             'typo3' => '12.4.22-13.4.99',
@@ -13,13 +17,5 @@ $EM_CONF[$_EXTKEY] = [
             'academic_persons' => '2.3.2',
             'academic_persons_edit' => '2.3.2',
         ],
-        'conflicts' => [
-        ],
-        'suggests' => [
-        ],
     ],
-    'description' => 'Adds some configuration for external users providers like Active Directory.',
-    'state' => 'beta',
-    'title' => 'FGTCLB: Academic Persons Sync',
-    'version' => '2.3.2',
 ];
